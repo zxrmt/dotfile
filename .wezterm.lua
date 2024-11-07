3729010e753a3da15077139b6720b47d9e43a5a5
zxrmt <49388235+zxrmt@users.noreply.github.com>
2024-11-07 15:48:54 +0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,10 +4,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.macos_window_background_blur = 20
 
-
+config.scrollback_lines = 10000
 -- For example, changing the color scheme:
 config.enable_scroll_bar = true
 config.hide_tab_bar_if_only_one_tab = true
