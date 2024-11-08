d417a8fa5182adef9d4f4e18ea4482b9123a556e
zxrmt <49388235+zxrmt@users.noreply.github.com>
2024-11-08 17:33:06 +0700

    Update term.lua

--- .config/nvim/lua/plugins/term.lua
+++ .config/nvim/lua/plugins/term.lua
@@ -3,10 +3,18 @@ return {
   config = function()
     require("toggleterm").setup({
       open_mapping = [[<c-\>]],
-      shade_terminals = false,
+      shade_terminals = true,
+      title_pos = "center",
+      border = "double",
       -- add --login so ~/.zprofile is loaded
       -- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
-      shell = "zsh --login",
+      shell = "zsh",
+      winbar = {
+        enabled = true,
+        name_formatter = function(term) --  term: Terminal
+          return string.rep("-", vim.o.columns * 4)
+        end
+      },
     })
   end,
   keys = {
@@ -14,7 +22,7 @@ return {
     { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
     {
       "<leader>td",
-      "<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>",
+      "<cmd>ToggleTerm size=10 dir=~/Desktop direction=horizontal border=curved<cr>",
       desc = "Open a horizontal terminal at the Desktop directory",
     },
   },
