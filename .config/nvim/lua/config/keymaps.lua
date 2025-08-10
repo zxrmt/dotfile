6e5151901f637a61d10ec19e8a66051b3623fef2
zxrmt <49388235+zxrmt@users.noreply.github.com>
2025-08-11 06:57:28 +0700

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,8 +8,10 @@ vim.api.nvim_set_keymap("n", "<C-m>", ":w<CR>", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "f", ":Telescope grep_string", { noremap = true, silent = true })
 
-
 vim.api.nvim_set_keymap("n", "q", ":q!<CR>", { noremap = true, silent = true })
 -- Hex editing stuff
-vim.api.nvim_set_keymap('n', '<leader>he', ':%!xxd<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>hu', ':%!xxd -r<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>he", ":%!xxd<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>hu", ":%!xxd -r<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-u>", "5k", { noremap = true, silent = true, desc = "Move up 5 lines" })
+vim.keymap.set("n", "<C-d>", "5j", { noremap = true, silent = true, desc = "Move up 5 lines" })
