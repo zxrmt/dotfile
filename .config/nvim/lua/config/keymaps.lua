cc3315c829cf8007db3c30a28277275f36ab792f
zx <zx@gmail.com>
2025-08-11 00:50:30 +0000

    adding key map go back

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -15,3 +15,7 @@ vim.api.nvim_set_keymap("n", "<leader>hu", ":%!xxd -r<CR>", { noremap = true, si
 
 vim.keymap.set("n", "<C-u>", "5k", { noremap = true, silent = true, desc = "Move up 5 lines" })
 vim.keymap.set("n", "<C-d>", "5j", { noremap = true, silent = true, desc = "Move up 5 lines" })
+
+-- Jump list navigation remapping
+vim.keymap.set("n", "<C-e>", "<C-o>", { noremap = true, silent = true, desc = "Go back in jump list" })
+vim.keymap.set("n", "<C-i>", "<C-i>", { noremap = true, silent = true, desc = "Go forward in jump list" })
