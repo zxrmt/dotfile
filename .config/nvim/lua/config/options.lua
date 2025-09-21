f2b7003144cb742af0bcbb3c6da9430f8cf881a3
zxrmt <49388235+zxrmt@users.noreply.github.com>
2025-09-21 21:59:56 +0700

    Update options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,3 +7,4 @@ vim.wo.wrap = true
 vim.wo.linebreak = true
 vim.wo.list = false -- extra option I set in addition to the ones in your question
 vim.diagnostic.disable = true
+vim.g.autoformat = false
